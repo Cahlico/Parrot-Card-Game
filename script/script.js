957dc0db7c8f9d6dc95c82038440a7c252f1d2a5
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-09-09 13:44:31 -0300

    gifs escolhidos aleatoriamente

--- script/script.js
+++ script/script.js
@@ -1,10 +1,39 @@
-numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
-numeroCartas = parseInt(numeroCartas);
+var numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
+var numeroCartas = parseInt(numeroCartas);
+var index;
+var cont;
+var imagensEscolhidas = [];
 
+// revisa se o numero escolhido esta de acordo com as condicoes
 while ((numeroCartas > 14) || (numeroCartas < 4) || (numeroCartas % 2 !== 0)) {
     numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
 }
 
-/*listaImagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+//array dos gifs a serem utilizados
+listaImagens = ["<img src='imagens/bobrossparrot.gif'>", "<img src='imagens/explodyparrot.gif'>", "<img src='imagens/fiestaparrot.gif'>", "<img src='imagens/metalparrot.gif'>", "<img src='imagens/revertitparrot.gif'>", "<img src='imagens/tripletsparrot.gif'>", "<img src='imagens/unicornparrot.gif'>"];
 
-document.querySelector("main").innerText = listaImagens[2];*/
+//disposicao de cartas com base na escolha do usuario
+for(index = 1; index <= numeroCartas; index++) {
+    document.querySelector("main > img:nth-child(" + index + ")").style.display = "inline-block";
+}
+
+//escolhe os gifs aleatoriamente para serem usados
+for(index = 0; index < (numeroCartas/2); index++) {
+    var valorAleatorio = Math.floor(Math.random() * listaImagens.length);
+    var imagemAleatoria = listaImagens[valorAleatorio];
+
+    for(cont = 0; cont < listaImagens.length; cont++) {
+        if(imagemAleatoria === imagensEscolhidas[cont]) {
+            valorAleatorio = Math.floor(Math.random() * listaImagens.length);
+            imagemAleatoria = listaImagens[valorAleatorio];
+            cont = 0;
+        }
+    }
+
+    imagensEscolhidas.push(imagemAleatoria);
+}
+
+
+
+
+//document.querySelector("main").innerHTML = listaImagens[2];
