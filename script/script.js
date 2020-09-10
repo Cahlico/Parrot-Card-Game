11e3dc7bde166482cff9d8803b3ee85713271e6a
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-09-10 19:31:43 -0300

    opcao de jogar novamente habilitada

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-var numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
-numeroCartas = parseInt(numeroCartas);
+var numeroCartas;
+var imagemAleatoria = "";
 var index;
 var cont;
 var listaImagens;
@@ -11,20 +11,32 @@ var cartaVirada;
 var cartaAnterior;
 var paresAcertados = 0;
 
-// revisa se o numero escolhido esta de acordo com as condicoes
-while ((numeroCartas > 14) || (numeroCartas < 4) || (numeroCartas % 2 !== 0)) {
-    numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
-}
-
 //array dos gifs a serem utilizados
 listaImagens = ["<img src='imagens/bobrossparrot.gif' class='verso'>", "<img src='imagens/explodyparrot.gif' class='verso'>", "<img src='imagens/fiestaparrot.gif' class='verso'>", "<img src='imagens/metalparrot.gif' class='verso'>", "<img src='imagens/revertitparrot.gif' class='verso'>", "<img src='imagens/tripletsparrot.gif' class='verso'>", "<img src='imagens/unicornparrot.gif' class='verso'>"];
 
+escolherNumeroCartas();
+
+// revisa se o numero escolhido esta de acordo com as condicoes
+function escolherNumeroCartas() {
+    numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
+    numeroCartas = parseInt(numeroCartas);
+
+    while ((numeroCartas > 14) || (numeroCartas < 4) || (numeroCartas % 2 !== 0)) {
+        numeroCartas = prompt("Quantas cartas você deseja?\n(escolha um número par entre 4 e 14)");
+    }
+
+    escolherCartas();
+}
+
 //escolhe os gifs aleatoriamente para serem usados
-for(index = 0; index < (numeroCartas/2); index++) {
-    var valorAleatorio = Math.floor(Math.random() * listaImagens.length);
-    var imagemAleatoria = listaImagens[valorAleatorio];
-      
-    imagensEscolhidas.push(verificaImagem()); //verifica uma imagem para nao ser escolhida mais de uma vez
+function escolherCartas() {
+    
+    for(index = 0; index < (numeroCartas/2); index++) {
+        var valorAleatorio = Math.floor(Math.random() * listaImagens.length);
+        imagemAleatoria = listaImagens[valorAleatorio];
+          
+        imagensEscolhidas.push(verificaImagem()); //verifica uma imagem para nao ser escolhida mais de uma vez
+    }
 }
 
 function verificaImagem() {
@@ -41,15 +53,16 @@ function verificaImagem() {
     
     posicao.push(imagemAleatoria); //adiciona duas de cada imagem no array posicao
     posicao.push(imagemAleatoria);
-    
+
     if (posicao.length === numeroCartas) {
         dispoeCartas(); //faz um shuffle no array posicao
     }
-
+    
     return imagemAleatoria;
 }
 
 function dispoeCartas() {
+
     var index = posicao.length;
     var variavelTemporaria;
     var indexAletorio;
@@ -116,6 +129,29 @@ function verificaPares(elemento, elementoAnterior) {
         
         if(paresAcertados === numeroCartas/2) {
             alert("Você venceu em " + rodadas + " rodadas!");
+            var irNovamente = prompt("Jogar denovo? (s/n)");
+            
+            if(irNovamente.toUpperCase() === "S" || irNovamente.toUpperCase() === "SIM") {
+                rodadas = 0;
+                paresAcertados = 0;
+                numeroCartas = 0;
+                imagemAleatoria = "";
+                index = 0;
+                cont = 0;
+                imagensEscolhidas = [];
+                posicao = [];
+                rodadas = 0;
+                paresAcertados = 0;
+                removeCartas();
+            }
         }
     }
+}
+
+function removeCartas() {
+    document.querySelector("ul").remove();
+    var main = document.querySelector("main");
+    novoUl = document.createElement("ul");
+    main.appendChild(novoUl);
+    escolherNumeroCartas();
 }
